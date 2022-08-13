e2781bc3f39854ad6bdc2db42bf5971fd89b96ff
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2022-08-13 03:51:44 -0400

    navbar & welcome

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
